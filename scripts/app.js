3b2c50aa4e86d2836213f810b45d28258d42f086
unknown <12sanketkumbhar@gmail.com>
2020-06-07 00:01:42 +0530

    Added button animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,25 @@
-const tags = ['creating things','coding','design','minimalism','sunsets','photography','colors','aesthetics','making art']
+const tags = ['creating things', 'coding', 'design', 'minimalism', 'sunsets', 'photography', 'colors', 'aesthetics', 'making art']
 
 let index = 0
 
-setInterval(()=>{
-    if(index === tags.length){
+setInterval(() => {
+    if (index === tags.length) {
         index = 0
     }
     document.getElementById('tag').innerText = tags[index];
     index++
 }, 3000)
 
-function toggleBottomBar(){
+function toggleBottomBar() {
     document.getElementById('bottom-bar').classList.toggle('active')
     document.getElementById('name-box').classList.toggle('active')
 }
+
+var upButton = document.getElementById('up-button');
+var rotate = 180;
+
+upButton.addEventListener('click', ()=> {
+    upButton.style = 'transform: rotate(' + rotate + 'deg); transition-duration: 500ms';
+    rotate +=180;
+})
+
